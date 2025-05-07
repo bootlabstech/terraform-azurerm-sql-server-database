a2f21ba897a826deff41d4c59e25c2bf6a058eb7
Bhargava Naidu <bhargava@bootlabstech.com>
2025-05-07 12:43:51 +0530

    udpated the version from 3.22.0 to 3.64.0

--- provider.tf
+++ provider.tf
@@ -3,11 +3,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.22.0"
+      version = "~> 3.64.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
